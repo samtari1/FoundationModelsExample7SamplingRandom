b96ae56a1be54b187b4a45fff3b4d86dfbbc4550
samtari1 <samtariyang@gmail.com>
2026-03-05 10:34:34 -0500

    Initial Commit

---
+++ FoundationModelsExample7SamplingRandom/FoundationModelsExample7SamplingRandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample7SamplingRandomApp.swift
+//  FoundationModelsExample7SamplingRandom
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsExample7SamplingRandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoundationModelsExample7SamplingRandomTests/FoundationModelsExample7SamplingRandomTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample7SamplingRandomTests.swift
+//  FoundationModelsExample7SamplingRandomTests
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import Testing
+@testable import FoundationModelsExample7SamplingRandom
+
+struct FoundationModelsExample7SamplingRandomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
